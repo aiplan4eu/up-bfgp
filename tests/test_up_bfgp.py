5e7b47c3308a271eb6ddeb85668472d651971ba9
js <jsego6@gmail.com>
2023-05-26 16:49:42 +0200

    Tests updated to use Fewshot planner

--- tests/test_up_bfgp.py
+++ tests/test_up_bfgp.py
@@ -6,23 +6,22 @@ import unittest
 from unified_planning.engines.results import PlanGenerationResultStatus
 import up_bfgp
 
+
 class BFGPtest(unittest.TestCase):
     def test_bfgp(self):
         """Testing the BFGP++ solvers can solve 3 different Gripper instances"""
-        with OneshotPlanner(name='bfgp', params={'program_lines': 15}) as bfgp:
-            reader = PDDLReader()
-            pddl_problem = reader.parse_problem('./gripper/domain.pddl', './gripper/p01.pddl')
-            result = bfgp.solve(pddl_problem)
-            self.assertEqual(result.status, PlanGenerationResultStatus.SOLVED_SATISFICING)
-            pddl_problem = reader.parse_problem('./gripper/domain.pddl', './gripper/p02.pddl')
-            result = bfgp.solve(pddl_problem)
-            self.assertEqual(result.status, PlanGenerationResultStatus.SOLVED_SATISFICING)
-            pddl_problem = reader.parse_problem('./gripper/domain.pddl', './gripper/p03.pddl')
-            result = bfgp.solve(pddl_problem)
-            self.assertEqual(result.status, PlanGenerationResultStatus.SOLVED_SATISFICING)
-            print(f'{bfgp.name} found a valid plan for {pddl_problem.name}!')
-            print(f'The plan is:')
-            print('\n'.join(str(x) for x in result.plan.actions))
+
+    with up.environment.get_environment().factory.FewshotPlanner(name='bfgp') as bfgp:
+        bfgp.set_arguments(program_lines=15)
+        pddl_problems = [PDDLReader().parse_problem('gripper/domain.pddl', f'gripper/p0{idx}.pddl') for idx in range(1, 4)]
+        # print(pddl_problem)
+        result = bfgp.solve(pddl_problems, output_stream=sys.stdout)
+        if all(r == PlanGenerationResultStatus.SOLVED_SATISFICING for r in result):
+            print(f'{bfgp.name} found a valid generalized plan!')
+            # print(f'The plan is:')
+            # print('\n'.join(str(x) for x in result.plan.actions))
+        else:
+            print('No generalized plan found!')
 
 
 if __name__ == "__main__":
