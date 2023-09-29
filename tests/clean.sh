a4eaa26b02da2bf116bca47e666e98ba7c1a8c11
Javier Segovia <jsego6@gmail.com>
2023-09-29 19:38:17 +0200

    Clean tests data updated

--- tests/clean.sh
+++ tests/clean.sh
@@ -1,1 +1,1 @@
-rm -rf experiments/ tmp/ plan.* test_gripper/ test_visitall/ test_spanner/ __pycache__/ test_miconic/
+rm -rf experiments/ tmp* plan.* test_gripper/ test_visitall/ test_spanner/ __pycache__/ test_miconic/
